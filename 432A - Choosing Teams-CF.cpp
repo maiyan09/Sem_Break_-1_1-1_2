268776a767d78797c6645fe5d679a79f10de0e63
maiyan09 <s.rahman.maiyan@gmail.com>
2025-07-28 20:37:27 +0600

    Create 432A - Choosing Teams-CF.cpp

---
+++ 432A - Choosing Teams-CF.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, k, count = 0;
+    cin >> n >> k;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        if (arr[i] <= 5-k) count++;
+    } 
+    cout << count/3 << endl;
+}
