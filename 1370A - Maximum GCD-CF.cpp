7879e0e1a4154419cf1fd0de4e61bc4ac9f90bac
maiyan09 <s.rahman.maiyan@gmail.com>
2025-08-15 20:40:04 +0600

    Create 1370A - Maximum GCD-CF.cpp

---
+++ 1370A - Maximum GCD-CF.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        int maxGCD = n / 2;
+        cout << maxGCD << endl;
+    }
+    return 0;
+}
