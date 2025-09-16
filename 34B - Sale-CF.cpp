bb9904f0af4df92bb4479435f4884d3409339501
maiyan09 <s.rahman.maiyan@gmail.com>
2025-09-16 20:11:33 +0600

    Create 34B - Sale-CF.cpp

---
+++ 34B - Sale-CF.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, m, sum = 0;
+    cin >> n >> m;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr+n);
+    for (int i = 0; i < n && m > 0; i++) {
+        if ((arr[i] < 0)) {
+            sum += arr[i];
+            m--;
+        }
+    }
+    cout << -sum << endl;
+    return 0;
+}
