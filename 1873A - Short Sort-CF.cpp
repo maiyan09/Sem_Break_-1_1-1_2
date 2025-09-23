0511c5833af39deb01567f53cd340aec8e29000a
maiyan09 <s.rahman.maiyan@gmail.com>
2025-09-23 21:55:33 +0600

    Create 1873A - Short Sort-CF.cpp

---
+++ 1873A - Short Sort-CF.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        string s; cin >> s;
+        if (s == "bca" || s == "cab") cout << "NO" << endl;
+        else cout << "YES" << endl;
+    }
+}
