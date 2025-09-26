a1ef0cf212f31275126c9b66c14cfa2d77b405df
maiyan09 <s.rahman.maiyan@gmail.com>
2025-09-26 15:13:33 +0600

    Create 266B - Queue at the School-CF.cpp

---
+++ 266B - Queue at the School-CF.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, t; cin >> n >> t;
+    string s; cin >> s;
+    for (int j = 1; j <= t; j++) {
+        for (int i = 0; i < n-1; ) {
+            if (s[i] == 'B' && s[i+1] == 'G') {
+                swap(s[i], s[i+1]);
+                i += 2;
+            }
+            else i++;
+        }
+    }
+    cout << s << endl;
+}
