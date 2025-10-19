949f028c0bbcba693105c4c05ab50d76426526f0
maiyan09 <s.rahman.maiyan@gmail.com>
2025-10-19 19:05:50 +0600

    Create 1772A - A+B?-CF.cpp

---
+++ 1772A - A+B?-CF.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while (t--) {
+       string s; cin >> s;
+        int sum = s[0]-'0' + s[2]-'0';
+        cout << sum << endl; 
+    }
+}
