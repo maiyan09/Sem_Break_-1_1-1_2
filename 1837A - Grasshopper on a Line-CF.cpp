a8d3c7dce25973f2a4364fb077d07fa73c9f9d90
maiyan09 <s.rahman.maiyan@gmail.com>
2025-10-31 09:43:02 +0600

    Implement Grasshopper on a Line solution

---
+++ 1837A - Grasshopper on a Line-CF.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        int x, k; cin >> x >> k;
+        if (x % k != 0) {
+            cout << "1" << endl << x << endl;
+        }
+        else {
+            cout << "2" << endl << "1 " << x-1 << endl;
+        }
+    }
+}
