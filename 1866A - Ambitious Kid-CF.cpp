755cebd5d42973f58d52f4e410976bde5c165c58
maiyan09 <s.rahman.maiyan@gmail.com>
2025-11-01 10:34:46 +0600

    Solution for 1866A - Ambitious Kid CodeForces
    Fix sorting and output of the smallest absolute value.

---
+++ 1866A - Ambitious Kid-CF.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        if (arr[i] < 0) arr[i] *= -1;
+    }
+  
+    sort(arr, arr+n);
+    cout << arr[0] << endl;
+    return 0;
+}
