76e851d4cddce16127a7bec15103435a903ab760
maiyan09 <s.rahman.maiyan@gmail.com>
2025-11-30 10:49:44 +0600

    Add Ideal Generator program to check even or odd

---
+++ A - Ideal Generator-CF.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        if (n%2== 0)
+            cout << "NO" << endl;
+        else
+            cout << "YES" << endl;
+    }
+
+    return 0;
+}
