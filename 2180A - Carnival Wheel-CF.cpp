9d1a3fd4da00d04d9aefd5fcb7392b1dab878357
maiyan09 <s.rahman.maiyan@gmail.com>
2025-12-31 11:12:37 +0600

    Add solution for Carnival Wheel problem
    Implement solution for the Carnival Wheel problem.

---
+++ 2180A - Carnival Wheel-CF.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        int l, a, b; cin >> l >> a >> b;
+        int ans = -1;
+        for (int i = 0 ; i < l; i++) {
+            ans = max(ans, (a+i*b)%l);
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
