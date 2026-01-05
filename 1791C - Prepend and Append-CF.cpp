45c6aff967676f985c40d34c1d65287021f0f976
maiyan09 <s.rahman.maiyan@gmail.com>
2026-01-05 11:31:13 +0600

    Add main function for string comparison problem

---
+++ 1791C - Prepend and Append-CF.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        string s; cin >> s;
+        int i = 0, j = n-1;
+        while (s[i] != s[j] && i <= j) {
+            i++; j--;
+        }
+        cout << j-i + 1 << endl;
+    }
+
+    return 0;
+}
