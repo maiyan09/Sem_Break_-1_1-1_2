4771bff360128ee89a4a0a145dbd14292656d0e7
maiyan09 <s.rahman.maiyan@gmail.com>
2026-01-26 12:07:46 +0600

    Implement solution for Maximum Multiple Sum problem

---
+++ 1985B - Maximum Multiple Sum-CF.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        int n, k; cin >> n;
+        
+        if (n == 3) cout << n << endl;
+        else cout << "2" << endl;
+    }
+
+    return 0;
+}
