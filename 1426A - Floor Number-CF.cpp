494f69a0034fcf3c58f178857a9b14fa2f9b3305
maiyan09 <s.rahman.maiyan@gmail.com>
2026-01-31 21:42:32 +0600

    Add solution for Floor Number problem

---
+++ 1426A - Floor Number-CF.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        int x, y; cin >> x >> y;
+        if (x == 1 || x == 2) {
+            cout << "1" << endl;
+            continue;
+        }
+        x -= 2;
+        if (x % y == 0) cout << x/y + 1 << endl;
+        else cout << x/y + 2 << endl;
+    }
+
+    return 0;
+}
